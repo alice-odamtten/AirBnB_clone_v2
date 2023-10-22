0cf15a0a12f4f41facf2dc015be0e3d53e7de1f9
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-10-21 23:09:43 -0700

    task 2

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    '''a function to display text'''
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''a function to display text'''
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
