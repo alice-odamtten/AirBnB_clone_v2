bb243fb9245a0594104a8dbedd458567d4d528f1
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-10-22 21:44:26 -0700

    state updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    if os.environ.get("HBNB_TYPE_STORAGE") == 'db':
+        name = Column('name', String(128), nullable=False)
+        cities = relationship("City", back_populates="state")
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            from models.__init__ import storage
+            from models.city import City
+
+            obj_list = []
+            strg = storage.all(City)
+            for key, value in strg.items():
+                if self.id == value.state_id:
+                    obj_list.append(value)
+            return obj_list
